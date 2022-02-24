9d8f0825d7be4802ffb1e0ffaae97377ec537e69
Aniwat Suttimanorat <aniwatsuttimanorat@gmail.com>
2022-02-24 23:14:44 +0700

    feat : add order repo test :17

--- src/main/java/com/example/demo/ordering/order/OrderRepository.java
+++ src/main/java/com/example/demo/ordering/order/OrderRepository.java
@@ -4,8 +4,11 @@ import com.example.demo.ordering.objects.UsersBasket;
 import com.example.demo.ordering.objects.UsersOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<UsersOrder,Integer> {
     Optional<UsersOrder> findByWhiskyOrder_CreditCardPayment_transactionId(String transactionId);
+
+    List<UsersOrder> findByShopper_username(String username);
 }
