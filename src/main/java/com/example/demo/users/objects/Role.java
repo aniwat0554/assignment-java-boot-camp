70a02f160d1492af6ff1206a4f7a94d319a3965e
Aniwat Suttimanorat <aniwatsuttimanorat@gmail.com>
2022-03-06 21:15:47 +0700

    feat : prepare required authentication class #21

---
+++ src/main/java/com/example/demo/users/objects/Role.java
@@ -0,0 +1,43 @@
+package com.example.demo.users.objects;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Role implements GrantedAuthority {
+
+    public static final String USER_ADMIN = "USER_ADMIN";
+    public static final String AUTHOR_ADMIN = "AUTHOR_ADMIN";
+    public static final String BOOK_ADMIN = "BOOK_ADMIN";
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    private String authority;
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+}
--- src/main/java/com/example/demo/users/objects/User.java
+++ src/main/java/com/example/demo/users/objects/User.java
@@ -1,19 +1,38 @@
 package com.example.demo.users.objects;
 
 import com.example.demo.shipment.Address;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-public class User{
+public class User implements UserDetails {
 
 	@OneToOne(optional = false,cascade= CascadeType.ALL)
 	private Address address;
 	private String username;
+	private final boolean enabled = true;
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private String password;
 
 	public User(Address address, String username) {
 		this.address = address;
 		this.username = username;
+	}
+
+	public User(Address address, String username,String password) {
+		this.address = address;
+		this.username = username;
+		this.password = password;
 	}
 
 	public User() {
@@ -22,6 +41,14 @@ public class User{
 	@Id
 	@GeneratedValue
 	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public void setAddress(Address address){
 		this.address = address;
@@ -35,7 +62,38 @@ public class User{
 		this.username = username;
 	}
 
+	@Override
+	public Set<Role> getAuthorities() {
+		return null;
+	}
+
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
 	public String getUsername(){
 		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return enabled;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return enabled;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return enabled;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return enabled;
 	}
 }
